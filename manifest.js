9cab40b7e69389827b2db50fe7a4b062d7e2eb68
Pauline Vos <pvos88@gmail.com>
2017-09-16 11:42:45 +0200

    Add jquery

---
+++ manifest.js
@@ -0,0 +1,13 @@
+{
+    "manifest_version": 2,
+    "name": "Thanks, Clippy",
+    "version": "0.1"
+    "content_scripts": [
+    {
+      "matches": [
+        "<all_urls>"
+      ],
+      "js": ["assets/js/jquery.min.js"]
+    }
+  ]      
+}
